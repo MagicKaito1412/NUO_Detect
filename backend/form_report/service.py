3472a1b681ade8b75fc797d1d0e8dabd385dd880
Alexey <7rick03ligh7@gmail.com>
2021-05-10 23:41:03 +0300

    get svg convert to jpg

--- backend/form_report/service.py
+++ backend/form_report/service.py
@@ -1,5 +1,7 @@
 from mailmerge import MailMerge
 from backend.form_report import app
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
 
 path = app.root_path.replace('\\', '/')
 doc_template = f"{path}/templates/doc_report_template.docx"
@@ -7,8 +9,18 @@ patient_template = f"{path}/templates/patient_report_template.docx"
 
 
 def doc_report(data):
+    svg = data.get('svg')
     patient_data = data.get('patient')
     ekgs_data = data.get('ekgs')
+    svg = '<svg' + svg.split('<svg')[-1]
+    with open(f"{path}/templates/img.svg", 'w') as f:
+        f.write(svg)
+    drawing = svg2rlg("img.svg")
+    sx, sy = 3
+    drawing.width, drawing.height = drawing.minWidth() * sx, drawing.height * sy
+    drawing.scale(sx, sy)
+    renderPM.drawToFile(drawing, "img.jpg", fmt="JPG")
+
     for item in ekgs_data:
         for key, value in item.items():
             item[key] = str(value)
